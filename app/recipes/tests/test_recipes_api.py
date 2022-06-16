5b5d8173e5855525e237b0f6e7a2f66385a95df3
unknown <ehsansarikhani8888@gmail.com>
2022-06-16 18:26:58 +0430

    Added recipe create endpoint

--- app/recipes/tests/test_recipes_api.py
+++ app/recipes/tests/test_recipes_api.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe, Tag, Ingredient
 from recipes.serializers import RecipeSerializer, RecipeDetailSerializer
+from decimal import Decimal
 
 RECIPES_URL = reverse("recipe:recipe-list")
 
@@ -89,18 +90,41 @@ class PrivateRecipesApiTests(TestCase):
 
         self.assertEqual(res.data, serializer.data)
 
-    # def test_create_tag(self):
-    #     payload = {"name": "some-name"}
-    #     self.client.post(TAGS_URL, payload)
-    #     exists = Tag.objects.filter(
-    #         user=self.user,
-    #         name=payload["name"]
-    #     ).exists()
-    #
-    #     self.assertTrue(exists)
-    #
-    # def test_create_tag_invalid(self):
-    #     payload = {"name": ""}
-    #     res = self.client.post(TAGS_URL, payload)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_create_recipe(self):
+        payload = {
+            "title": "some-name",
+            "time_minutes": 10,
+            "price": Decimal('9.99')
+        }
+        res = self.client.post(RECIPES_URL, payload)
+        recipe = Recipe.objects.get(id=res.data["id"])
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(recipe, key))
+
+    def test_create_recipe_with_tags_and_ingredients(self):
+        tag1 = sample_tag(user=self.user, name="name1")
+        tag2 = sample_tag(user=self.user, name="name2")
+        ingredient1 = sample_ingredient(user=self.user, name="name1")
+        ingredient2 = sample_ingredient(user=self.user, name="name2")
+        payload = {
+            "title": "some-name",
+            "tags": [tag1.id, tag2.id],
+            "ingredients": [ingredient1.id, ingredient2.id],
+            "time_minutes": 10,
+            "price": 9.99,
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        recipe = Recipe.objects.get(id=res.data["id"])
+        tags = recipe.tags.all()
+        ingredients = recipe.ingredients.all()
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(tags.count(), 2)
+        self.assertEqual(ingredients.count(), 2)
+        self.assertIn(tag1, tags)
+        self.assertIn(tag2, tags)
+        self.assertIn(ingredient1, ingredients)
+        self.assertIn(ingredient2, ingredients)
--- app/recipes/views.py
+++ app/recipes/views.py
@@ -38,3 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return serializers.RecipeDetailSerializer
         return serializers.RecipeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
